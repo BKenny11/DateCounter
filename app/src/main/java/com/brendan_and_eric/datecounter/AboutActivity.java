0306668a5b879008e223ff10fce0f9d3d6ef194a
BKenny11 <bwk8958@rit.edu>
2015-09-28 09:47:12 -0400

    about screen fixed

--- app/src/main/java/com/brendan_and_eric/datecounter/AboutActivity.java
+++ app/src/main/java/com/brendan_and_eric/datecounter/AboutActivity.java
@@ -1,9 +1,11 @@
 package com.brendan_and_eric.datecounter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -14,4 +16,28 @@ public class AboutActivity extends AppCompatActivity {
     }
 
 
+    public void close(View view){
+        finish();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+        }
+
+        if (id == R.id.addButton){
+            Intent intent = new Intent(this, AddActivity.class);
+
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
