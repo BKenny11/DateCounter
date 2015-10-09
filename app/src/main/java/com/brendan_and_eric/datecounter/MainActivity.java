216221a6a7c6a1ece8739f3e0a3a8aab5c662f20
BKenny11 <bwk8958@rit.edu>
2015-10-09 16:08:22 -0400

    Two bugs corrected

--- app/src/main/java/com/brendan_and_eric/datecounter/MainActivity.java
+++ app/src/main/java/com/brendan_and_eric/datecounter/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 DateTime dater2 = new DateTime(date2);
                 int DaysBetween = Days.daysBetween(dater1, dater2).getDays();
 
-                String DaysBetweenString = String.valueOf(Math.abs(DaysBetween));
+                String DaysBetweenString = String.valueOf(Math.abs(DaysBetween+1));
 
 
 
@@ -194,7 +194,8 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, AboutActivity.class);
             startActivity(intent);
-        }else{
+        }
+        if (id == R.id.instruction_settings){
             Intent intent = new Intent(this, InstructionsActivity.class);
             startActivity(intent);
         }
